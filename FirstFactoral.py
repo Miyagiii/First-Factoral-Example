34cf5c4bc1ab67d4d7af4ee82c4b1e30030ff6e9
TheSpaceCowboy <deadpool42534@gmail.com>
2017-08-12 04:08:07 +0100

    First Factoral
    This is my solution for finding the first factoral of a number.

---
+++ FirstFactoral.py
@@ -0,0 +1,32 @@
+def FirstFactorial(num): 
+
+    nums = []#contains all the numbers which will be used to times togther
+    
+    for x in range(0,num):#adds all the numbers we will be using to times into an array
+        
+        nums.append(x)
+        
+    for x in range(1,len(nums)):#this times togther all the numbers
+        
+        num = num*nums[x]
+        
+    return num
+     
+print(FirstFactorial(int(input())))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
